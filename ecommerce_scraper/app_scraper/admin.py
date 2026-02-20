5c248d99612ffc584eedeb8c41520c2802799cf5
Thais Zorawski <thaiszorawski@gmail.com>
2026-02-20 00:09:08 -0300

    feat(admin): implements admin product view

--- ecommerce_scraper/app_scraper/admin.py
+++ ecommerce_scraper/app_scraper/admin.py
@@ -4,4 +4,13 @@ from .models import Store, Category, Subcategory, Product
 admin.site.register(Store)
 admin.site.register(Category)
 admin.site.register(Subcategory)
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name", "subcategory", "store", 
+        "check_date", "regular_price", "offer_price", "is_offer"
+    )
+    list_filter = ("store", "check_date", "is_offer")
+    search_fields = ("name", "ean")
+    readonly_fields = ("check_date",)
